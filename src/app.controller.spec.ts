47a68d5ef3b94581a4602392714f1a63872b7127
WilsonPM96 <wilson.ramos@live.com>
2018-05-13 10:46:27 -0500

    cambios

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,18 +3,18 @@ import { INestApplication } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 describe('AppController', () => {
-  let app: TestingModule;
+    let app: TestingModule;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-    }).compile();
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.root()).toBe('Hello World!');
+    beforeAll(async () => {
+        app = await Test.createTestingModule({
+            controllers: [AppController],
+        }).compile();
     });
-  });
+
+    describe('root', () => {
+        it('should return "Hello World!"', () => {
+            const appController = app.get<AppController>(AppController);
+            expect(appController.root()).toBe('Hello World!');
+        });
+    });
 });
